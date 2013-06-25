08e0bb49572c25290ea647699161c2948def2c4b
Damon Oehlman <damon.oehlman@gmail.com>
2013-06-25 10:34:05 +1000

    Added media import, and main convenience package

---
+++ index.js
@@ -0,0 +1,8 @@
+// export peer connection
+exports.PeerConnection = require('./peerconnection');
+
+// export media
+exports.media = require('./media');
+
+// export the signaller
+exports.signaller = require('./signaller');
