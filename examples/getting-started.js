5232faf5c4ee543ddb5db715fa3273e38a3bca52
Damon Oehlman <damon.oehlman@gmail.com>
2014-01-13 10:45:33 +1100

    Tweaked example so it works nicely in firefox too

--- examples/getting-started.js
+++ examples/getting-started.js
@@ -6,34 +6,33 @@ var localMedia = media();
 // render the local media to the document body
 localMedia.render(document.body);
 
-// look for friends
-signaller.on('peer:announce', function(data) {
-  // create a peer connection for our new friend
-  var pc = rtc.createConnection();
+// capture local media first as firefox
+// will want a local stream and doesn't support onnegotiationneeded event
+localMedia.once('capture', function(localStream) {
+  // look for friends
+  signaller.on('peer:announce', function(data) {
+    // create a peer connection for our new friend
+    var pc = rtc.createConnection();
 
-  // couple our connection via the signalling channel
-  var monitor = rtc.couple(pc, data.id, signaller);
+    // couple our connection via the signalling channel
+    var monitor = rtc.couple(pc, data.id, signaller);
 
-  if (localMedia.stream) {
-    pc.addStream(localMedia.stream);
-  }
-  else {
-    localMedia.once('capture', function(stream) {
-      pc.addStream(stream);
+    // add the stream to the connection
+    pc.addStream(localStream);
+
+    // once the connection is active, log a console message
+    monitor.once('active', function() {
+      console.log('connection active to: ' + data.id);
     });
-  }
 
-  // once the connection is active, log a console message
-  monitor.once('active', function() {
-    console.log('connection active to: ' + data.id);
+    // when the peer connection receives a remote stream render it to the
+    // screen
+    pc.onaddstream = function(remoteStream) {
+      media(remoteStream).render(document.body);
+    };
   });
 
-  // when the peer connection receives a remote stream render it to the
-  // screen
-  pc.onaddstream = function(stream) {
-    media(stream).render(document.body);
-  };
+  // announce ourself in the rtc-getting-started room
+  signaller.announce({ room: 'rtc-getting-started' });
 });
 
-// announce ourself in the rtc-getting-started room
-signaller.announce({ room: 'rtc-getting-started' });
