e483303bd16921db860e33677dc128d0056fcc5a
Damon Oehlman <damon.oehlman@gmail.com>
2014-03-19 15:38:40 +1100

    Removed debug logging

--- test/capture-close.js
+++ test/capture-close.js
@@ -10,7 +10,7 @@ var scope = [];
 var messengers = [];
 var dcs = [];
 
-require('cog/logger').enable('monitor');
+// require('cog/logger').enable('monitor');
 
 test('create peer connections', function(t) {
   t.plan(2);
