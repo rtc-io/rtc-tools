3ab82b1b829f6da7ffc42449ecf581473edb7773
Damon Oehlman <damon.oehlman@gmail.com>
2014-04-07 16:31:23 +1000

    Added monitor test to check monitor in isolation from couple

---
+++ test/monitor.js
@@ -0,0 +1,28 @@
+var test = require('tape');
+var peerpair = require('peerpair');
+var monitor = require('../monitor');
+var rtc = require('../');
+var pcs = [];
+var monitors = [];
+
+test('use peerpair to create a connected set of peers', function(t) {
+  t.plan(1);
+  pcs = peerpair();
+  pcs.events.once('connected', t.pass.bind(t, 'connected'));
+  pcs.connect();
+});
+
+test('create monitors for the connections', function(t) {
+  t.plan(2);
+  monitors = pcs.map(monitor);
+  t.equal(typeof monitors[0].on, 'function', 'monitor:0 created');
+  t.equal(typeof monitors[0].on, 'function', 'monitor:1 created');
+});
+
+test('monitor connection close', function(t) {
+  t.plan(pcs.length);
+  pcs.forEach(function(pc, index) {
+    monitors[index].once('closed', t.pass.bind(t, 'closed connection: ' + index));
+    pc.close();
+  });
+});
