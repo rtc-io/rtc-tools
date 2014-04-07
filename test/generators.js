ac4f3f1bab2b5e3b0dd3097b71f08eec0dc59e83
Damon Oehlman <damon.oehlman@gmail.com>
2014-04-07 10:49:21 +1000

    Cleanup generator tests

--- test/generators.js
+++ test/generators.js
@@ -1,44 +1,6 @@
 var generators = require('../generators');
 var test = require('tape');
-var testConstraints = {
-  optional: [
-    { DtlsSrtpKeyAgreement: true }
-  ],
-  mandatory: {
-    OfferToReceiveAudio: true,
-    OfferToReceiveVideo: true
-  }
-};
-
-test('parseflags on undefined', function(t) {
-  t.plan(1);
-  t.ok(generators.parseFlags(), 'ok');
-});
-
-test('video flag recognized', function(t) {
-  t.plan(1);
-  t.ok(generators.parseFlags({ video: true }).indexOf('video') >= 0, 'found');
-});
-
-test('falsey video flag ignored', function(t) {
-  t.plan(1);
-  t.ok(generators.parseFlags({ video: false }).indexOf('video') < 0, 'ok');
-});
-
-test('video flag recognized (UPPERCASE OK)', function(t) {
-  t.plan(1);
-  t.ok(generators.parseFlags({ VIDEO: true }).indexOf('video') >= 0, 'found');
-});
-
-test('audio flag recognized', function(t) {
-  t.plan(1);
-  t.ok(generators.parseFlags({ audio: true }).indexOf('audio') >= 0, 'found');
-});
-
-test('data flag recognized', function(t) {
-  t.plan(1);
-  t.ok(generators.parseFlags({ data: true }).indexOf('data') >= 0, 'found');
-});
+var freeice = require('freeice');
 
 test('generate config', function(t) {
   var config;
@@ -54,4 +16,13 @@ test('extend config', function(t) {
   t.plan(2);
   t.ok(config = generators.config({ a: 1 }), 'generated config');
   t.equal(config.a, 1, 'setting preserved');
+});
+
+test('extend config with iceServers', function(t) {
+  var config;
+  var testServers = freeice();
+
+  t.plan(2);
+  t.ok(config = generators.config({ iceServers: testServers }), 'generated config');
+  t.deepEqual(config.iceServers, testServers, 'servers matched');
 });
